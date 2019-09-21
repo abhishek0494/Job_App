9595935b3b5c03fb5e0fc09c315553b8d6d445d6
abhishek0494 <abhishek@discoverdollar.com>
2019-09-21 06:34:13 -0500

    route modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,9 @@ import {JobComponent} from './job/job.component';
 import {AppComponent} from './app.component'
 import {SsoComponent} from './sso/sso.component';
 const routes: Routes = [
-  { path: '', component: AppComponent,
-      children: [
-         { path: 'home', component: SsoComponent },
-         {path : 'jobs' , component: JobComponent}
-      ]
-  }
+  { path: '', component: AppComponent},
+  { path: 'home', component: SsoComponent },
+  {path : 'jobs' , component: JobComponent}
 ];
 
 @NgModule({
