68419386b8bd0df10042e2a0a51bea9e532e2f42
abhishek0494 <abhishek@discoverdollar.com>
2019-09-21 03:20:24 -0500

    node server added

---
+++ server.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/job-app'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/job-app/index.html'));
+});
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
